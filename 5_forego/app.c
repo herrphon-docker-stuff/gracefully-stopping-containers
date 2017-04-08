866d2c3c8b577b875e15bc0100195b7abf4b5479
Alexander von Renteln <alexander.vonrenteln@advantest.com>
2017-04-08 14:13:34 +0200

    ...

--- 5_forego/app.c
+++ 5_forego/app.c
@@ -9,30 +9,34 @@ void term(int signum)
 {
     done = 1;
 }
- 
+
+
 int main(int argc, char *argv[])
 {
     struct sigaction action;
     memset(&action, 0, sizeof(struct sigaction));
     action.sa_handler = term;
     sigaction(SIGTERM, &action, NULL);
- 
+
+    fprintf(stderr, "hello world\n");
+
     int loop = 0;
     while (!done)
     {
-        int t = sleep(3);
+        int t = sleep(5);
         /* sleep returns the number of seconds left if *
          * interrupted */
         while (t > 0)
         {
-            printf("Loop run was interrupted with %d "
-                   "sec to go, finishing...\n", t);
+            fprintf(stderr, "Loop run was interrupted with %d sec to go, finishing...\n", t);
             t = sleep(t);
         }
-        printf("Finished loop run %d.\n", loop++);
+        fprintf(stderr, "Finished loop run %d.\n", loop++);
     }
- 
-    printf("done.\n");
+
+    fprintf(stderr, "done.\n");
     return 0;
 }
+
+
 
--- 5_forego/run.sh
+++ 5_forego/run.sh
@@ -4,12 +4,14 @@ docker build -t asdf .
 
 ID=$(docker run -d asdf)
 
-sleep 6
+sleep 12
 docker kill --signal=SIGTERM $ID
 
 docker logs -f $ID
 
 echo "Exit status should be 0: $(docker inspect -f '{{.State.ExitCode}}' $ID)"
+
+docker ps -a
 
 docker rm $ID
 
