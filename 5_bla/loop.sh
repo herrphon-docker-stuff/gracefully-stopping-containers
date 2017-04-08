8fa7284c4581eac3011804532c18de71e7343868
Alexander von Renteln <alexander.vonrenteln@advantest.com>
2017-04-08 12:52:50 +0200

    add forego

--- 5_bla/loop.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-trap 'echo "exiting"; exit 0' SIGTERM
-
-while true; do :; done
-
---
+++ 5_forego/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+
+docker build -t asdf .
+
+ID=$(docker run -d asdf)
+
+sleep 6
+docker kill --signal=SIGTERM $ID
+
+docker logs -f $ID
+
+echo "Exit status should be 0: $(docker inspect -f '{{.State.ExitCode}}' $ID)"
+
+docker rm $ID
+
+
